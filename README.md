d076f32f4e3315f33d6bab0508b7a8849a570661
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-05-16 17:52:34 -0300

    Initial commit
    Add `README` with installation and configuration of `commitlint`.
    
    Add `LICENSE`.
    
    Add main file containing our rules.

---
+++ README.md
@@ -0,0 +1,21 @@
+# commitlint-config-rocketseat
+
+Shareable [`commitlint`](https://github.com/marionebl/commitlint) config used by Rocketseat.
+
+## Getting started
+
+Installing:
+
+```sh
+# npm
+npm i -D commitlint-config-rocketseat
+
+# yarn
+yarn add -D commitlint-config-rocketseat
+```
+
+Apply the config to `commitlint`:
+
+```sh
+echo "module.exports = {extends: ['rocketseat']};" > commitlint.config.js
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+  rules: {
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [2, "always", ["chore", "ci", "docs", "feat", "fix", "test"]],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-max-length": [2, "always", 50],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "body-leading-blank": [2, "always"],
+    "body-max-line-length": [2, "always", 72],
+    "footer-leading-blank": [2, "always"],
+    "footer-max-line-length": [2, "always", 72]
+  }
+};
